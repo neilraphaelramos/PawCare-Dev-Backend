71071e9744c5f08d2b393e46110865071d57ac2d
neilraphaelramos <neilraphael.ramos4@gmail.com>
2025-11-06 20:42:01 +0800

    Fixed Issue on STMP

--- config/mailer.js
+++ config/mailer.js
@@ -2,10 +2,15 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,           
+  secure: true,          
   auth: {
     user: process.env.EMAIL,
     pass: process.env.APP_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
