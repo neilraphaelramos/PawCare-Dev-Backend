b056192114616b8f44a47532e30ceafd93a25f0a
neilraphaelramos <neilraphael.ramos4@gmail.com>
2025-11-06 20:59:10 +0800

    Fix 2

--- config/mailer.js
+++ config/mailer.js
@@ -3,14 +3,11 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,           
-  secure: true,          
+  port: 587,           
+  secure: false,          
   auth: {
     user: process.env.EMAIL,
     pass: process.env.APP_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
